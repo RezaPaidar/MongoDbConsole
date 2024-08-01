fd27b949abbf5d418112f244f3bfd5fb5ade994c
RezaPaidar <reza.paidar@outlook.com>
2024-08-01 17:20:17 +0330

    feat-- InsertMany

--- MongoDbConsole/Program.cs
+++ MongoDbConsole/Program.cs
@@ -10,12 +10,32 @@ var database = client.GetDatabase("bank");
 var accounts = database.GetCollection<Accounts>("account");
 
 
-var document = new Accounts
+//var document = new Accounts
+//{
+//    AccountId = "MD123",
+//    AccountHolder = "Reza Paidar",
+//    AccountType = "checking",
+//    Balance = 50352434,
+//};
+
+//accounts.InsertOne(document);
+
+
+var AcoountA = new Accounts
 {
-    AccountId = "MD123",
-    AccountHolder = "Reza Paidar",
-    AccountType = "checking",
-    Balance = 50352434,
+    AccountId = "MD124",
+    AccountHolder = "Sina Dardar",
+    AccountType = "Checking",
+    Balance = 1200005,
+
+};
+var AcoountB = new Accounts
+{
+    AccountId = "MD125",
+    AccountHolder = "Aida Sharifi",
+    AccountType = "Checking",
+    Balance = 1800002,
+
 };
 
-accounts.InsertOne(document);
+accounts.InsertMany(new List<Accounts>() { AcoountA, AcoountB });
