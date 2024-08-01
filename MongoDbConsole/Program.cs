7104cbdc76c9188e8b4536803181ba425b746f16
RezaPaidar <reza.paidar@outlook.com>
2024-08-01 12:19:37 +0330

    Add project files.

---
+++ MongoDbConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+var mongodbUrl = "mongodb://<username>:<password>@<hostname>:<port>/<database>?<options>";
+
+var client = new MongoClient(mongodbUrl);
+var dbList = client.ListDatabaseNames().ToList();
+
+Console.WriteLine("This list of database on this server is: ");
+foreach (var db in dbList)
+{
+    Console.WriteLine(db);
+}
+
+
+var document = new BsonDocument
+{
+    {"accountId","MD123"},
+    {"acountHolder","Reza Paidar"},
+    {"acountType","checking"},
+    {"balance","50352434"}
+};
