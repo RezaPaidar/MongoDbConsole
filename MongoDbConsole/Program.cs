94ba790825837d836a2791e9cafe9404e3797489
RezaPaidar <reza.paidar@outlook.com>
2024-08-01 17:26:40 +0330

    feat -- Find query by linq

--- MongoDbConsole/Program.cs
+++ MongoDbConsole/Program.cs
@@ -21,21 +21,33 @@ var accounts = database.GetCollection<Accounts>("account");
 //accounts.InsertOne(document);
 
 
-var AcoountA = new Accounts
+//var AcoountA = new Accounts
+//{
+//    AccountId = "MD124",
+//    AccountHolder = "Sina Dardar",
+//    AccountType = "Checking",
+//    Balance = 1200005,
+
+//};
+//var AcoountB = new Accounts
+//{
+//    AccountId = "MD125",
+//    AccountHolder = "Aida Sharifi",
+//    AccountType = "Checking",
+//    Balance = 1800002,
+
+//};
+
+//accounts.InsertMany(new List<Accounts>() { AcoountA, AcoountB });
+
+//var account = accounts.Find(x=>x.AccountId == "MD123").FirstOrDefault();
+
+//Console.WriteLine(account.AccountHolder);
+
+var accs = accounts.Find(x=>x.AccountType == "Checking")
+    .SortByDescending(x=>x.Balance).Limit(3).ToList();
+
+foreach (var acc in accs)
 {
-    AccountId = "MD124",
-    AccountHolder = "Sina Dardar",
-    AccountType = "Checking",
-    Balance = 1200005,
-
-};
-var AcoountB = new Accounts
-{
-    AccountId = "MD125",
-    AccountHolder = "Aida Sharifi",
-    AccountType = "Checking",
-    Balance = 1800002,
-
-};
-
-accounts.InsertMany(new List<Accounts>() { AcoountA, AcoountB });
+    Console.WriteLine(acc.AccountHolder);
+}
